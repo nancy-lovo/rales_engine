7fe7ab967b6be7d713da60012feb7080ce9fbc85
Nancy Lee <srlee713@gmail.com>
2019-10-02 16:56:33 -0600

    Add associations to merchant model along with passing test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Merchant, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
+
+  describe "relationships" do
+    it { should have_many :items }
+    it { should have_many :invoices }
+    it { should have_many(:invoice_items).through(:invoices) }
+  end
+
 end
