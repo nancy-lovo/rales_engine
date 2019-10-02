517144af48bc624bb31e814f2c0a057dde341b33
Nancy Lee <srlee713@gmail.com>
2019-10-02 16:51:10 -0600

    Add associations to item model along with passing test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, :description, :merchant_id
+  validates_presence_of :name, :description, :merchant_id, :created_at, :updated_at
   validates_numericality_of :unit_price, greater_than_or_equal_to: 0, only_integer: true
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  belongs_to :merchant
 end
