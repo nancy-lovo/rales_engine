81f72d6f4fc97f1dc4f724c4a8c7c46128bd5b18
Nancy Lee <srlee713@gmail.com>
2019-10-02 23:16:54 -0600

    Modify validation requirement in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,5 @@
 class Transaction < ApplicationRecord
-  validates_presence_of :invoice_id, :result
-  validates_numericality_of :credit_card_number, greater_than: 0, only_integer: true
+  validates_presence_of :invoice_id, :result, :credit_card_number
   validates :credit_card_expiration_date, presence: true, allow_blank: true
 
   belongs_to :invoice
