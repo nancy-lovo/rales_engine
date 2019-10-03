5a5ada42b7dd3ed279471ae981aae20756afdb8d
Nancy Lee <srlee713@gmail.com>
2019-10-02 23:16:40 -0600

    Change credit card number datatype from integer to string in transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_044900) do
+ActiveRecord::Schema.define(version: 2019_10_03_051301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_10_03_044900) do
 
   create_table "transactions", force: :cascade do |t|
     t.integer "invoice_id"
-    t.bigint "credit_card_number"
+    t.string "credit_card_number"
     t.integer "credit_card_expiration_date"
     t.string "result"
     t.datetime "created_at", precision: 6, null: false
