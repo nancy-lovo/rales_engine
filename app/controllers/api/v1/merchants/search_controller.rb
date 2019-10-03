bdba55e2cc26d0cfd8de5a3603c837b66c2f511e
Nancy Lee <srlee713@gmail.com>
2019-10-02 21:01:09 -0600

    Add index action in all controllers to add find_all functionality

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::Merchants::SearchController < ApplicationController
     render json: MerchantSerializer.new(Merchant.find_by(merchant_params))
   end
 
+  def index
+    render json: MerchantSerializer.new(Merchant.where(merchant_params))
+  end
+
   private
 
   def merchant_params
