a3dce373f87a323052a3de9d2fe6921aa504f02f
Nancy Lee <srlee713@gmail.com>
2019-10-02 23:08:33 -0600

    Add find_all tests in items request spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -96,4 +96,122 @@ describe "Items API" do
     expected = Item.find(item["data"]["id"]).updated_at
     expect(expected).to eq(updated_at)
   end
+
+  it "can find all instances by id" do
+    id = create(:item).id
+
+    get "/api/v1/items/find_all?id=#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(1)
+
+    expected = item["data"].all? { |hash| hash["attributes"]["id"] == id }
+    expect(expected).to eq(true)
+  end
+
+  it "can find all instances by name" do
+    name = "cookie"
+    items = create_list(:item, 3)
+    Item.update_all(name: name)
+    fruit = create(:item, name: "fruit")
+
+    get "/api/v1/items/find_all?name=#{name}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(3)
+
+    expected = item["data"].all? { |hash| hash["attributes"]["name"] == name }
+    expect(expected).to eq(true)
+  end
+
+  it "can find all instances by description" do
+    description = "very sweet"
+    items = create_list(:item, 3)
+    Item.update_all(description: description)
+    bad_item = create(:item, description: "very bad")
+
+    get "/api/v1/items/find_all?description=#{description}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(3)
+
+    expected = item["data"].all? { |hash| hash["attributes"]["description"] == description }
+    expect(expected).to eq(true)
+  end
+
+  it "can find all instances by unit_price" do
+    unit_price = "10.00"
+    items = create_list(:item, 3)
+    Item.update_all(unit_price: unit_price)
+    item_2 = create(:item, unit_price: '15.00')
+
+    get "/api/v1/items/find_all?unit_price=#{unit_price}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(3)
+
+    expected = item["data"].all? { |hash| hash["attributes"]["unit_price"] == unit_price }
+    expect(expected).to eq(true)
+  end
+
+  it "can find all instances by merchant id" do
+    merchant_id = 1
+    items = create_list(:item, 3)
+    Item.update_all(merchant_id: merchant_id)
+
+    get "/api/v1/items/find_all?merchant_id=#{merchant_id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(3)
+
+    expected = item["data"].all? { |hash| hash["attributes"]["merchant_id"] == merchant_id }
+    expect(expected).to eq(true)
+  end
+
+  it "can find all instances by created at" do
+    items = create_list(:item, 3)
+    created_at = items.first.created_at
+
+    get "/api/v1/items/find_all?created_at=#{created_at}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(3)
+
+    expected = item["data"].all? { |hash| Item.find(hash["id"]).created_at = created_at }
+    expect(expected).to eq(true)
+  end
+
+  it "can find all instances by updated at" do
+    items = create_list(:item, 3)
+    updated_at = items.first.updated_at
+
+    get "/api/v1/items/find_all?updated_at=#{updated_at}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+
+    expect(item["data"].count).to eq(3)
+
+    expected = item["data"].all? { |hash| Item.find(hash["id"]).updated_at = updated_at }
+    expect(expected).to eq(true)
+  end
 end
