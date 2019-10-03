d91122b7b10166d92ea4ae43c2a9a6fc68b8e749
Nancy Lee <srlee713@gmail.com>
2019-10-02 22:57:27 -0600

    Modify validation requirement in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, :description, :merchant_id
-  validates_numericality_of :unit_price, greater_than_or_equal_to: 0, only_integer: true
+  validates_presence_of :name, :description, :merchant_id, :unit_price
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
