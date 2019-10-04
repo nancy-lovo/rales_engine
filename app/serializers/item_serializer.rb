f02833a6e5c1f96e301288138c02dabb07d1ebf7
Nancy Lee <srlee713@gmail.com>
2019-10-03 21:11:02 -0600

    Add unit price formatting in item and invoice_item serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,13 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description, :unit_price, :merchant_id
+
+  attribute :unit_price do |object|
+    up = object['unit_price']
+    if up > 100
+      (up / 100.to_f).to_s
+    else
+      '%.2f' % up
+    end
+  end
 end
