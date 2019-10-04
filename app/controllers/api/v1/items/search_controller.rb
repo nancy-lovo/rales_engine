0d24332f17169eb5e74dcc8a7876cd00821c7894
Nancy Lee <srlee713@gmail.com>
2019-10-03 21:10:39 -0600

    Add unit price formatting within search params in item and invoice_item controllers

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,15 +1,21 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    render json: ItemSerializer.new(Item.find_by(item_params))
+    render json: ItemSerializer.new(Item.order_items.find_by(item_params))
   end
 
   def index
-    render json: ItemSerializer.new(Item.where(item_params))
+    render json: ItemSerializer.new(Item.order_items.where(item_params))
   end
 
   private
 
   def item_params
+    if params['unit_price']
+      if params['unit_price'].include? "."
+        params['unit_price'] = params['unit_price'].tr('.', '').to_i
+      end
+      params['unit_price'] = params['unit_price'].to_i
+    end
     params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 end
