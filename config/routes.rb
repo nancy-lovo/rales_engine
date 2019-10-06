279e7656699c9de452d2b5b174eb6900af92ada2
nancylee713 <srlee713@gmail.com>
2019-10-06 14:18:14 -0600

    Add relationship endpoints for Transaction model

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
         get '/random', to: "random#show"
+        get '/:id/invoice', to: "invoice#show"
       end
 
       resources :customers, only: [:index, :show]
